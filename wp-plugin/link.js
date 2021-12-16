3f06bba95b864924ed06e7d8638e26f91805d399
aizwellenstan <39293145+aizwellenstan@users.noreply.github.com>
2021-12-16 17:54:08 +0800

    Update link.js

--- wp-plugin/link.js
+++ wp-plugin/link.js
@@ -1,17 +1,7 @@
 function addLink(link,id) {
   elem = document.getElementById(id)
-  if (elem) return 0;
-  var a = document.createElement('a'); 
-                  
-  // Create the text node for anchor element.
-  var link = document.createTextNode("link");
-
-  // Append the text node to anchor element.
-  a.appendChild(link);
-
   // Set the href property.
-  a.href = link+id; 
-  elem.prepend(a)
+  elem.href = link+id
 }
 
 var link = "https://shop.mng-intl.com/index.php/about/#"
