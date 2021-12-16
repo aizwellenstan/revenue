4a3bf52b5d9bab99221acef5277826743dd809bf
aizwellenstan <39293145+aizwellenstan@users.noreply.github.com>
2021-12-16 18:45:05 +0800

    Update make-div-link.js

--- wp-plugin/make-div-link.js
+++ wp-plugin/make-div-link.js
@@ -1,13 +1,16 @@
 function addLink(link,id) {
 	console.log(id)
 	var element = document.getElementById(id);
-	classList = "elementor-column elementor-inner-column";
+	element.onclick = function() {
+		location.href = link;
+	};
+	/*classList = "elementor-column elementor-inner-column";
 	var parent = element.parentNode;
 	var aTag = document.createElement('a');
 	aTag.setAttribute('href', link);
-	aTag.classList = classList;
+	aTag.style.display = "block";
 	parent.replaceChild(aTag, element);
-	aTag.appendChild(element);
+	aTag.appendChild(element);*/
 }
 
 var link = "https://shop.mng-intl.com/index.php/about/#"
